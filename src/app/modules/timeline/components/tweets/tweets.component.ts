80d15a5384cfb5679b7dec16877ffaf2a0567552
Miguel Rebelatto <miguel.rebelatto@cwi.com.br>
2021-11-17 01:29:51 -0300

    feat(tweets): field date; var now; order list; style(tweets): content item; pipe date field; icon remove

--- src/app/modules/timeline/components/tweets/tweets.component.ts
+++ src/app/modules/timeline/components/tweets/tweets.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Subscription } from 'rxjs';
+import { TweetEventEnum } from 'src/app/modules/shared/enums/eventList.enum';
 import { Tweet } from 'src/app/modules/shared/models/tweet.model';
 import { TweetService } from 'src/app/modules/shared/services/tweet.service';
+import { getDifferenceDatetimeInSeconds } from 'src/app/modules/shared/util/date.util';
 import { ModalConfirmationComponent } from '../modal-confirmation/modal-confirmation.component';
 
+const ONE_MINUTE = 60;
+const FIFTEEN_SECONDS = 30000;
 @Component({
   selector: 'app-tweets',
   templateUrl: './tweets.component.html',
@@ -12,26 +16,53 @@ import { ModalConfirmationComponent } from '../modal-confirmation/modal-confirma
 })
 export class TweetsComponent implements OnInit, OnDestroy {
 
+  now: Date = new Date();
   tweets: Array<Tweet> = [];
   subscription: Subscription;
+  interval: any;
 
   constructor(
     private tweetService: TweetService,
     private modalService: NgbModal
   ) { }
 
+  get list() {
+    return this.tweets.sort((a, b) => a.createdAt < b.createdAt ? 1 : -1);
+  }
+
+  isShowSeconds(item: Tweet): boolean {
+    let diff = getDifferenceDatetimeInSeconds(this.now, new Date(item.createdAt))
+    return diff <= ONE_MINUTE;
+  }
+
+  getTextSeconds(item: Tweet): string {
+    let seconds = getDifferenceDatetimeInSeconds(this.now, new Date(item.createdAt))
+    return Math.ceil(seconds) <= 1 ? `Agora` : `${Math.ceil(seconds)} s`
+  }
+
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
+    clearInterval(this.interval);
   }
 
   ngOnInit(): void {
     this.tweets = [...this.tweetService.getTweets()];
     this.observableListEvent();
+    this.initInterval();
+  }
+
+  initInterval() {
+    this.interval = setInterval(() => {
+      this.now = new Date();
+    }, FIFTEEN_SECONDS);
   }
 
   observableListEvent() {
     this.subscription = this.tweetService.onChange.subscribe(data => {
-      this.tweets = [...this.tweetService.getTweets()];
+      if (data.event == TweetEventEnum.ADDED)
+        this.tweets.push(data.tweet)
+      else
+        this.tweets.splice(this.tweets.findIndex(i => i.id === data.tweet.id), 1)
     })
   }
 
